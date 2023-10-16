d5a230f396132cb33101a583b99f70e115fc83cc
Olena <72007221+Oleva-abr@users.noreply.github.com>
2023-10-16 19:05:51 +0300

    added trendings

--- moviestime/src/app/pages/home/home.component.ts
+++ moviestime/src/app/pages/home/home.component.ts
@@ -11,14 +11,24 @@ export class HomeComponent implements OnInit {
   constructor(private service: MovieApiServiceService) { }
 
   bannerApiData: any = []
+  trendingMovieData: any = []
 
   ngOnInit(): void {
     this.bannerData();
+    this.trendingData();
   }
 
   bannerData() {
     this.service.bannerApiData().subscribe((data) => {
       this.bannerApiData = data.results;
+
+    })
+  }
+
+  trendingData() {
+    this.service.trendingMovieApiData().subscribe((trendings) => {
+
+      this.trendingMovieData = trendings.results;
 
     })
   }
--- moviestime/src/app/service/movie-api-service.service.ts
+++ moviestime/src/app/service/movie-api-service.service.ts
@@ -16,4 +16,16 @@ export class MovieApiServiceService {
     return this.http.get(`${this.baseUrl}/trending/all/week?api_key=${this.apiKey}`);
   }
 
+  trendingMovieApiData(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/trending/movie/day?api_key=${this.apiKey}`);
+
+  }
+
+
+  getSearchMovie(data: any): Observable<any> {
+    console.log(data, 'movie#');
+
+    return this.http.get(`${this.baseUrl}/search/movie?api_key=${this.apiKey}&query=${data.movieName}`);
+  }
+
 }

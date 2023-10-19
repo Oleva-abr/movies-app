a3cacb76a6278d6fe66e42463193747d7b48fb02
Olena <72007221+Oleva-abr@users.noreply.github.com>
2023-10-19 14:05:55 +0300

    filled homepage

--- moviestime/src/app/pages/home/home.component.ts
+++ moviestime/src/app/pages/home/home.component.ts
@@ -12,10 +12,24 @@ export class HomeComponent implements OnInit {
 
   bannerApiData: any = []
   trendingMovieData: any = []
+  actionMovieData: any = []
+  adventureMovieData: any = []
+  animationMovieData: any = []
+  comedyMovieData: any = []
+  documentaryMovieData: any = []
+  fictionMovieData: any = []
+  thrillerMovieData: any = []
 
   ngOnInit(): void {
     this.bannerData();
     this.trendingData();
+    this.actionMovie();
+    this.adventureMovie();
+    this.comedyMovie();
+    this.animationMovie();
+    this.documentaryMovie();
+    this.sciencefictionMovie();
+    this.thrillerMovie();
   }
 
   bannerData() {
@@ -32,5 +46,59 @@ export class HomeComponent implements OnInit {
 
     })
   }
+
+  actionMovie() {
+    this.service.fetchActionMovies().subscribe((result) => {
+      this.actionMovieData = result.results;
+    });
+  }
+
+
+
+
+  adventureMovie() {
+    this.service.fetchAdventureMovies().subscribe((result) => {
+      this.adventureMovieData = result.results;
+    });
+  }
+
+
+
+  animationMovie() {
+    this.service.fetchAnimationMovies().subscribe((result) => {
+      this.animationMovieData = result.results;
+    });
+  }
+
+
+
+  comedyMovie() {
+    this.service.fetchComedyMovies().subscribe((result) => {
+      this.comedyMovieData = result.results;
+    });
+  }
+
+
+  documentaryMovie() {
+    this.service.fetchDocumentaryMovies().subscribe((result) => {
+      this.documentaryMovieData = result.results;
+    });
+  }
+
+
+
+  sciencefictionMovie() {
+    this.service.fetchScienceFictionMovies().subscribe((result) => {
+      this.fictionMovieData = result.results;
+    });
+  }
+
+
+  thrillerMovie() {
+    this.service.fetchThrillerMovies().subscribe((result) => {
+      this.thrillerMovieData = result.results;
+    });
+  }
+
 
 }
